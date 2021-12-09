81fec317844ec86efab2ebe3cc75416d3214795c
lavrushka00 <vadim-panevin@mail.ru>
2021-12-09 18:22:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const FunctComponent = (props) => {
+  let registredDate =
+    props.registredAt.getDate() +
+    " " +
+    montoToStr(props.registredAt.getMonth()) +
+    " " +
+    props.registredAt.getFullYear();
+
+  return (
+    <div className="profile">
+      <h1>
+        Привет, <b>{props.name}</b>{" "}
+      </h1>
+      <h2>дата регистрации: {registredDate}</h2>
+    </div>
+  );
+};
+
+class ClassComponent extends React.Component {
+  render() {
+    let registredDate =
+      this.props.registredAt.getDate() +
+      " " +
+      montoToStr(this.props.registredAt.getMonth()) +
+      " " +
+      this.props.registredAt.getFullYear();
+
+    return (
+      <div className="profile">
+        <h1>
+          Привет, <b>{this.props.name}</b>{" "}
+        </h1>
+
+        <h2>дата регистрации: {registredDate}</h2>
+      </div>
+    );
+  }
+}
+
+function montoToStr(num) {
+  return num > 12 || num < 1
+    ? null
+    : "января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря".split(
+        ","
+      )[num - 1];
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <ClassComponent name="Вася" registredAt={new Date(2021, 5, 22)} />
+
+      <FunctComponent name="Вася" registredAt={new Date(2021, 5, 22)} />
     </div>
   );
 }
