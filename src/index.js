fff63eca33b672bfd0a7331c82b078a246b8cc6b
lavrushka00 <vadim-panevin@mail.ru>
2021-12-11 13:58:40 +0300

    дз #4

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 //import App from './tutorial-1/App';
 //import App from './toturial-2/App';
-import App from './tutorial-3/App';
-
+//import App from './tutorial-3/App';
+import App from './tutorial-4/App';
 
 ReactDOM.render(
     <App />,
